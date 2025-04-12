58ee3ba8a09edda81deef92a1dbbd5251c109096
Mightt-gifty <giftymvula9@gmail.com>
2025-04-12 11:52:53 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,10 +6,10 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=0L0SiBjR#dFgJ6FtnS1tv3cY0fdyC16kawU3Diec9QkA3UXl2Wd8' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=xalz0ShY#_phULvxfA64XZmNjF9OiFdyBTV4HvKsZhmFjlb5tn4A' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '260777021038' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
-PREFIX: process.env.PREFIX || '!' ,
+PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
 MODE: process.env.MODE === undefined ? 'nonbutton' : process.env.MODE,
